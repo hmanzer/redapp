5f1021b805e17f650c7d9ede4850e466709ca4be
Humayun Manzer <humayun.manzer@outlook.com>
2023-06-11 12:00:34 +0800

    edited ip and ami

--- infrastructure/frontend/main.tf
+++ infrastructure/frontend/main.tf
@@ -17,14 +17,14 @@ module "redapp_frontend" {
   vpc_id                            = module.redapp_vpc.vpc_id
   private_subnet_ids                = [module.redapp_vpc.private_subnet_1_id]
   public_subnet_ids                 = [module.redapp_vpc.public_subnet_1_id]
-  ssh_whitelist_ipv4                = ["52.221.154.19/32"]   #myip                                                                                                                 # List of IPs to be whitelisted for SSH access, default Dynasty VPN
+  ssh_whitelist_ipv4                = ["202.185.236.177/32"]   #myip                                                                                                                 # List of IPs to be whitelisted for SSH access, default Dynasty VPN
   project_name                      = "hmz-redapp"                                                                                                                    # be as short and clear as possible
   s3_frontend_artifacts_bucket_name = "hmz-redapp-frontend"
   environment                       = var.environment
   auto_traffic_rerouting            = "true"
   description_of_project            = "redapp simple website"
   key_name                          = "devops-sg"
-  ami_image_id                      = "ami-00b4480c8529303fe" # Debian 10 hardened
+  ami_image_id                      = "ami-0df7a207adb9748c7" # Debian 10 hardened
   instance_type_frontend            = "t3a.small"
   init_flag                         = "true"          #First time?
   is_public                         = "true"
